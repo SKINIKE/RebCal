6f7eac7a1faee9a58ccc14e913377cc5bf4f80e2
ski <sunnike1010@gmail.com>
2022-11-18 00:24:15 +0900

    AssetViewModel 추가 /. 조회한 데이터 화면에 보여주기

--- RebCal/Model/AssetModel.swift
+++ RebCal/Model/AssetModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Asset: Codable{
-    var market: String = "KRW-BTC"
+    var market: String?
     var tradeDate: String?
     var tradeTime: String?
     var tradeDateKst: String?
@@ -34,49 +34,3 @@ struct Asset: Codable{
     var lowest52WeekDate: String?
     var timestamp: CLong? //api문서의 데이터 타입대로 선언
 }
-
-
-//api호출 예제
-//// [URL 지정 및 파라미터 값 지정 실시]
-//var urlComponents = URLComponents(string: "https://api.upbit.com/v1/ticker?")
-//let market = "KRW"
-//let ticker = "BTC"
-//let markets_value = market + "-" + ticker
-//let paramQuery_markets = URLQueryItem(name: "markets", value: markets_value)
-//urlComponents?.queryItems?.append(paramQuery_markets) // 파라미터 지정
-//
-//var requestURL = URLRequest(url: (urlComponents?.url)!)
-//       requestURL.httpMethod = "GET" // GET
-//       requestURL.addValue("application/x-www-form-urlencoded; charset=utf-8;", forHTTPHeaderField: "Content-Type") // GET
-////print("url : ", requestURL)
-//
-//// [http 요쳥을 위한 URLSessionDataTask 생성]
-//let dataTask = URLSession.shared.dataTask(with: requestURL) { (data, response, error) in
-//
-//    // [error가 존재하면 종료]
-//    guard error == nil else {
-//        print("fail : ", error?.localizedDescription ?? "")
-//        return
-//    }
-//
-//    // [status 코드 체크 실시]
-//    let successsRange = 200..<300
-//    print(successsRange)
-//    guard let statusCode = (response as? HTTPURLResponse)?.statusCode, successsRange.contains(statusCode)
-//    else {
-//        print("error : ", (response as? HTTPURLResponse)?.statusCode ?? 0)
-//        print("msg : ", (response as? HTTPURLResponse)?.description ?? "")
-//        return
-//    }
-//
-//    // [response 데이터 획득, Asset객체로 json을 파싱]
-//    let decoder = JSONDecoder()
-//    decoder.keyDecodingStrategy = .convertFromSnakeCase
-//    if let asset = try? decoder.decode([Asset].self, from: data!) {
-//        print(asset[0])
-//    }
-//}
-//
-//// network 통신 실행
-//dataTask.resume()
-//
--- RebCal/View/AddAssetView.swift
+++ RebCal/View/AddAssetView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct AddAssetView: View {
+    
+    @ObservedObject var assetViewModel = AssetViewModel()
+    @State var assetTicker: String = ""
+    
     var body: some View {
-        NavigationView(){
-            Text("자산을 추가합니다.")
+        VStack{
+            Text(assetViewModel.selectedAsset)
+            HStack{
+                TextField("Ticker", text: $assetTicker)
+                .frame(width: 200, height: 20)
+                .padding()
+                Button("검색"){
+                    assetViewModel.selectAsset()
+                }
+                .frame(width:50, height: 20)
+                .padding()
+            }
         }
     }
 }
